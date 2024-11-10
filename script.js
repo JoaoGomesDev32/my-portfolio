d50b6113aaffedb74938c986040f91b1a16fae1e
João Silva <joaogomesdev32@gmail.com>
2024-11-10 17:51:34 +0000

    :lipstick: img backgrounds particles

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
 document.querySelectorAll('.nav-link').forEach(link => {
     link.addEventListener('click', function(e) {
-      e.preventDefault();
-      const targetId = this.getAttribute('href').substring(1);
-      document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
     });
-  });
-  
+});
+
   // Função para rolar a página para o topo
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
-  }
-  
+}
+
   // Mostrar o botão quando o usuário rolar para baixo 100px
-  window.onscroll = function() {
+window.onscroll = function() {
     const scrollTopBtn = document.getElementById("scrollTopBtn");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-      scrollTopBtn.style.display = "block";
+        scrollTopBtn.style.display = "block";
     } else {
-      scrollTopBtn.style.display = "none";
+        scrollTopBtn.style.display = "none";
     }
-  };
-  
+};   
+
+particlesJS('particles-js', {
+    particles: {
+        number: { value: 80, density: { enable: true, value_area: 800 } },
+        color: { value: '#ffffff' },
+        shape: {
+            type: 'circle',
+            stroke: { width: 0, color: '#000000' },
+        },
+        opacity: { value: 0.5, random: false },
+        size: { value: 3, random: true },
+        line_linked: { enable: true, distance: 150, color: '#ffffff', opacity: 0.4, width: 1 },
+        move: { enable: true, speed: 6, direction: 'none', random: false, straight: false, out_mode: 'out' },
+    },
+    interactivity: {
+        detect_on: 'canvas',
+        events: { onhover: { enable: true, mode: 'repulse' }, onclick: { enable: true, mode: 'push' } },
+        modes: {
+            repulse: { distance: 100 },
+            push: { particles_nb: 4 },
+        },
+    },
+    retina_detect: true,
+});
